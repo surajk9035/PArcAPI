426250b48ad85d554629ef86e5c38a290807110a
Manish Premi <manish.p@roofandfoor.com>
2017-02-12 18:57:06 +0530

    Uploading and saving into DB

--- models/upload.js
+++ models/upload.js
@@ -2,15 +2,34 @@
  * Created by manish on 12/2/17.
  */
 var cloudinary = require('../helpers/cloudinary').cloudinary;
+var dbconfig = require('../models/dbconfig');
 module.exports = {
 
     imageUpload: function (req, res, err) {
-       console.log(req.body);
         if (req.header('X-FUTZ-SEC') == 'SorryForDelay-GetBackToYouSoon'){
-            cloudinary.uploader.upload(req.files.myImage.path,function(result) {
-                console.log(result)
-                res.json(result);
-            });
+            if(req.files.myImage.path){
+                cloudinary.uploader.upload(req.files.myImage.path,function(result) {
+                    console.log(result);
+                    var userId  =   req.body.userId;
+                    var imgDesc =   req.body.imgDesc;
+                    var albumId =   req.body.albumId;
+                    var imgUrl  =   result.url;
+                    var imgSUrl =   result.secure_url;
+                    var imgPId  =   result.public_id;
+                    var cDate   =   result.created_at;
+                    var query = "insert into ??  values(?,?,?,?,?,?,?,?)";
+                    var table = ["parcer_images" , "" , albumId, "", imgDesc ,cDate, 1, imgUrl, imgSUrl];
+                    query = dbconfig.msql.format(query, table);
+                    console.log(query);
+                    dbconfig.connection.query(query, function (err, rows) {
+                        if (err) {
+                            res.json({"status": "failure", "data": err});
+                        } else {
+                            res.json({"status": "Success", "data":"Congo You did it !!"});
+                        }
+                    })
+                });
+            }
         } else {
             res.json({"status": "failure", "data": "Wrong Security Code"});
         }
